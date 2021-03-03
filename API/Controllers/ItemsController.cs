a3201987e89e133b15fd3648f210154c5b83ec3e
m.gadou <m.gadou@EGY-PMT-EMP-26>
2021-03-03 13:10:10 +0200

    working on apis

--- API/Controllers/ItemsController.cs
+++ API/Controllers/ItemsController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Infrastructure.Db;
 using API.Models.Dto;
+using API.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,38 @@ namespace API.Controllers
             var stepItems = step.Items;
 
             return stepItems.Select(i => new ItemDto { Id = i.Id, Description = i.Description, Title = i.Title }).ToList();
+        }
+
+        [HttpPost]
+        public async Task<int> AddNewItem(CreateNewItemDto input)
+        {
+            var step = await _dbContext.Steps.Include(s => s.Items).Where(s => s.Id == input.StepId).SingleAsync();
+            step.Items.Add(new Item { Description = input.Description, Title = input.Title });
+
+            await _dbContext.SaveChangesAsync();
+
+
+            return step.Items.OrderByDescending(i => i.Id).First().Id;
+        }
+
+        [HttpPost]
+        public async Task EditItem(EditItemDto input)
+        {
+            var item = await _dbContext.Items.SingleAsync(i => i.Id == input.Id);
+
+            item.Title = input.Title;
+            item.Description = input.Description;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveItem(int itemId)
+        {
+            var item = await _dbContext.Items.SingleAsync(i => i.Id == itemId);
+
+            _dbContext.Items.Remove(item);
+
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
