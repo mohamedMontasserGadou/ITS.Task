448424557bb14c3678577a8361f66f1887495765
m.gadou <m.gadou@EGY-PMT-EMP-26>
2021-03-03 20:23:36 +0200

    Adding seed

---
+++ API/Infrastructure/Seed/SeedDataHelper.cs
@@ -0,0 +1,56 @@
+﻿using API.Infrastructure.Db;
+using API.Models.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Seed
+{
+    public static class SeedDataHelper
+    {
+        public static IHost Seed(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<AppDbContext>())
+                {
+                    if (appContext.Steps.Any())
+                        return host;
+
+                    appContext.Steps.Add(CreateDefaultData());
+                    appContext.SaveChanges();
+                }
+            }
+            return host;
+        }
+
+
+        private static Step CreateDefaultData()
+        {
+            return new Step
+            {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        Description = "Test",
+                        Title = "Test",
+                    },
+                    new Item
+                    {
+                        Description = "Test",
+                        Title = "Test",
+                    },
+                    new Item
+                    {
+                        Description = "Test",
+                        Title = "Test",
+                    }
+                }
+            };
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,7 @@
+using API.Infrastructure.Seed;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace API
 {
@@ -13,7 +9,7 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Seed().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
