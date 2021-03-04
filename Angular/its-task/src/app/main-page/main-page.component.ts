c1068aaec96c1f94ff49afc905ff771cb4ccc559
m.gadou <m.gadou@EGY-PMT-EMP-26>
2021-03-04 09:50:11 +0200

    adding stepper

--- Angular/its-task/src/app/main-page/main-page.component.ts
+++ Angular/its-task/src/app/main-page/main-page.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ItemDto } from '../Dto/ItemDto';
 import { ItemsService } from '../Services/items.service';
-
+import { CdkStepper, CdkStepperModule  } from '@angular/cdk/stepper';
+import { Directionality } from '@angular/cdk/bidi';
 @Component({
   selector: 'main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  providers: [{ provide: CdkStepper, useExisting: MainPageComponent }]
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent extends CdkStepper  implements OnInit {
 
   items: ItemDto[] = [];
-  constructor(private itemsService: ItemsService) { }
+  constructor(private itemsService: ItemsService,
+    _dir: Directionality, 
+    _changeDetectorRef: ChangeDetectorRef) {
+    super(_dir,_changeDetectorRef);
+  }
+
+  onClick(index: number): void {
+    this.selectedIndex = index;
+  }
 
   ngOnInit() {
     this.itemsService.GetItems(1).subscribe(data => this.items = data);
